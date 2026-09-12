ef6d5f809b51e0a0093bbc621958e02a66f8ce71
emerson-creator <emerson.albornoz@correounivalle.edu.co>
2026-09-11 23:04:17 -0500

    refactor: update Footer component

--- src/components/modules/landing/Footer.tsx
+++ src/components/modules/landing/Footer.tsx
@@ -7,8 +7,7 @@ export function Footer() {
     <footer className="bg-primary text-primary-foreground py-4">
       <div className="container mx-auto px-4">
         <p className="text-center text-sm">
-          &copy; {new Date().getFullYear()} Prism Store. Todos los derechos
-          reservados.
+          &copy; {new Date().getFullYear()} Prism Store. All rights reserved.
         </p>
       </div>
     </footer>
